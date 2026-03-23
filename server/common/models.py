a67926a993a3306eded968fa8b7e081de7b81369
BautistaBoeri <bboeri@fi.uba.ar>
2026-03-23 18:07:57 -0300

    fix: elimino codigo que no se utiliza mas

--- server/common/models.py
+++
@@ -1,7 +0,0 @@
-class Bet:
-    def __init__(self, first_name: str, last_name: str, document: str, birthdate: str, number: str):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.document = document
-        self.birthdate = birthdate
-        self.number = number

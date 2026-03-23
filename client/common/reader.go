18c683e937d3e96bfcb481fece034b80bf9ce106
BautistaBoeri <bboeri@fi.uba.ar>
2026-03-23 18:58:16 -0300

    fix: elimino responsabilidad del reader sobre el limite batches

--- client/common/reader.go
+++ client/common/reader.go
@@ -5,40 +5,24 @@ import (
 	"io"
 )
 
-const (
-	MAXBATCHSIZEBYTES = 8096
-)
-
-// BetReader es un wrapper sobre csv.Reader que mantiene el estado
-// de las apuestas leídas que no cupieron en el batch anterior.
+// BetReader es un wrapper sobre csv.Reader
 type BetReader struct {
-	reader      *csv.Reader
-	leftoverBet *Bet
+	reader *csv.Reader
 }
 
 // NewBetReader crea una nueva instancia de BetReader
 func NewBetReader(r *csv.Reader) *BetReader {
 	return &BetReader{
-		reader:      r,
-		leftoverBet: nil,
+		reader: r,
 	}
 }
 
 // ReadBatch lee un bloque de apuestas desde el archivo CSV empaquetado.
-// El batch cortará si alcanza 'maxAmount' O si excedería los 8000 bytes.
+// El batch cortará si alcanza 'maxAmount' o el fin del archivo.
 func (br *BetReader) ReadBatch(maxAmount int) ([]Bet, error) {
 	var batch []Bet
-	currentBatchSize := 3 // 3 bytes de header del protocolo de batch
 
-	// 1. Si nos quedó una apuesta del batch anterior que no entraba, la agregamos primero
-	if br.leftoverBet != nil {
-		betSize := 2 + len(br.leftoverBet.FirstName) + len(br.leftoverBet.LastName) + len(br.leftoverBet.Document) + len(br.leftoverBet.Birthdate) + len(br.leftoverBet.Number) + 4
-		batch = append(batch, *br.leftoverBet)
-		currentBatchSize += betSize
-		br.leftoverBet = nil // Ya la usamos, la limpiamos
-	}
-
-	for i := len(batch); i < maxAmount; i++ {
+	for i := 0; i < maxAmount; i++ {
 		record, err := br.reader.Read()
 		if err != nil {
 			if err == io.EOF {
@@ -55,20 +39,7 @@ func (br *BetReader) ReadBatch(maxAmount int) ([]Bet, error) {
 				Birthdate: record[3],
 				Number:    record[4],
 			}
-
-			// Calculamos el tamaño: 2 bytes de largo + la data + comas
-			betSize := 2 + len(bet.FirstName) + len(bet.LastName) + len(bet.Document) + len(bet.Birthdate) + len(bet.Number) + 4
-
-			// Si al sumar esta apuesta nos pasamos de seguridad (~8KB), la guardamos en la instancia para el PRÓXIMO batch
-			if currentBatchSize+betSize > MAXBATCHSIZEBYTES {
-				br.leftoverBet = &bet
-				// Retornamos nil porque no hay error, el único motivo de corte es que el batch está lleno
-				return batch, nil
-			}
-
-			// Si entra, la metemos y seguimos
 			batch = append(batch, bet)
-			currentBatchSize += betSize
 		}
 	}
 
